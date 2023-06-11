e01330668472995322f8a068982fd81b5d10d9c2
Rounak_Biswal <130887717+Rounak-Biswal@users.noreply.github.com>
2023-06-11 23:42:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var a;
+function show(){
+    document.getElementById('hidden').style.display = 'block';
+    return a=0;
+}
+var b;
+function hide(){
+    document.getElementById('hidden').style.display = 'none';
+    return b=0;
+}
